0c7381d56dc280c0a8e5ebe4f4bacffee7d67bb7
Diego Matheus <152340543+DiegoMatheusS@users.noreply.github.com>
2024-11-26 19:33:19 +0000

    uptadre login

--- obj/Release/net8.0/ECOCRIA.AssemblyInfo.cs
+++ obj/Release/net8.0/ECOCRIA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECOCRIA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830f81e414a2454f35abdba6a29912f8c2ff4735")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f48dc4833185b971bb3a3fa70920dfba80baa1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECOCRIA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECOCRIA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
