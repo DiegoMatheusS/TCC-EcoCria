e3764a5da1dca895e25a005e81cd009da1024823
Diego Matheus <152340543+DiegoMatheusS@users.noreply.github.com>
2026-03-11 18:16:54 +0000

    Publicação

--- Program.cs
+++ Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSomee"));
+    
 });
 
 // 3. Injeção de Dependências
